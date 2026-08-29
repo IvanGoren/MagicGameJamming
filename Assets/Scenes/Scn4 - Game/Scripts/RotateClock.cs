20775132dbb9cc969a4e35fd5d73eb5799658e61
AlvaRock8 <alfranconetti@hotmail.com>
2026-08-29 17:53:41 -0300

    AHHHHHHH reloj parcial

---
+++ Assets/Scenes/Scn4 - Game/Scripts/RotateClock.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [Tooltip("Degrees to rotate per second. Use 6 for realistic ticking.")]
+    public float speed = 0.8f; 
+
+    void Update()
+    {
+        // Smoothly rotates the UI asset clockwise on the Z-axis
+        transform.Rotate(0f, 0f, -speed * Time.deltaTime);
+    }
+}
